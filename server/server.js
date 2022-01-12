f3514811fcc377419c77990ece3f66067f8111ca
Rafael "Duck" Arroyo <87622570+DuckArroyo@users.noreply.github.com>
2022-01-12 12:59:01 -0800

    updated server 'running'

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
+const PORT = process.env.PORT || 3003;
+const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json);
 
 app.listen(PORT, () => {
-    console.log("test", PORT);
-})
+  console.log('http://localhost:', PORT);
+});
