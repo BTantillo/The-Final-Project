01b8f9d81888308e863a51d8cbe5093429c1cddc
Rafael "Duck" Arroyo <87622570+DuckArroyo@users.noreply.github.com>
2022-01-12 13:05:07 -0800

    added apollo elements

--- server/server.js
+++ server/server.js
@@ -1,7 +1,18 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
+
+
 
 const PORT = process.env.PORT || 3003;
 const app = express();
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json);
