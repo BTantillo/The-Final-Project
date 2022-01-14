65250bda602d98d0f0d90eed4d850ecbae7c7401
Rafael "Duck" Arroyo <87622570+DuckArroyo@users.noreply.github.com>
2022-01-13 20:12:31 -0800

    JWT/adding auth

--- server/schemas/resolver.js
+++ server/schemas/resolver.js
@@ -1,4 +1,6 @@
 const { User, Event } = require('../models/');
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
@@ -26,8 +28,20 @@ const resolvers = {
     
       return user;
     },
-    login: async () => {
-
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+    
+      if (!user) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+    
+      const correctPw = await user.isCorrectPassword(password);
+    
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+    
+      return user;
     }
   }
 };
