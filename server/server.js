167a2afb18cd62aee066320559097889fa889273
Rafael "Duck" Arroyo <87622570+DuckArroyo@users.noreply.github.com>
2022-01-14 11:41:36 -0800

    clean code

--- server/server.js
+++ server/server.js
@@ -28,10 +28,10 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+//! To activate graphQL comment this out
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
 
 db.once('open', () => {
   app.listen(PORT, () => {
