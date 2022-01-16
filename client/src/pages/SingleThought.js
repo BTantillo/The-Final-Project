7304e7af73b23ede315537b3dc707881837f3fdc
Rafael "Duck" Arroyo <87622570+DuckArroyo@users.noreply.github.com>
2022-01-15 16:17:11 -0800

    single thought update

--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -1,41 +1,41 @@
 import React from 'react';
 import { useParams } from 'react-router-dom'
 import { useQuery } from '@apollo/client'
-// import { QUERY_THOUGHT } from '../utils/queries'
+import { QUERY_EVENT } from '../utils/queries'
 import ReactionList from '../components/ReactionList'
 import ReactionForm from '../components/ReactionForm'
 import Auth from '../utils/auth'
 
-const SingleThought = props => {
-  const { id: thoughtId } = useParams();
+const SingleEvent = props => {
+  const { id: eventId } = useParams();
 
-  const { loading, data } = useQuery(QUERY_THOUGHT, {
-    variables: { id: thoughtId }
+  const { loading, data } = useQuery(QUERY_EVENT, {
+    variables: { id: eventId }
   })
 
-  const thought = data?.thought || {};
+  const event = data?.event || {};
 
   if(loading) {
     return <div>Loading...</div>
   }
-  console.log(thoughtId); 
+  console.log(eventId); 
   return (
     <div>
       <div className="card mb-3">
         <p className="card-header">
           <span style={{ fontWeight: 700 }} className="text-light">
-            {thought.username}
+            {event.username}
           </span>{' '}
-          thought on {thought.createdAt}
+          event on {event.createdAt}
         </p>
         <div className="card-body">
-          <p>{thought.thoughtText}</p>
+          <p>{event.eventText}</p>
         </div>
       </div>
-      {thought.reactionCount > 0 && <ReactionList reactions={thought.reactions} />}
-      {Auth.loggedIn() && <ReactionForm thoughtId={thought._id} />}
+      {event.reactionCount > 0 && <ReactionList reactions={event.reactions} />}
+      {Auth.loggedIn() && <ReactionForm eventId={event._id} />}
     </div>
   );
 };
 
-export default SingleThought;
+export default SingleEvent;
