b71aa0abaf34641731c40d9a8dacc0005fac8cb5
Shayne Lemons <lemonsshayne6@gmail.com>
2022-01-16 16:25:00 -0800

    add css

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="w-100 mt-auto bg-secondary p-4">
-      <div className="container">&copy;2020 by Ben Tantillo, Duck Arroyo, Jared Chesebro + Shayne Lemons</div>
+      <div className="container">&copy;2020 by Ben Tantillo</div>
     </footer>
   );
 };
